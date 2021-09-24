ea7af0d658d6bf78728b1d0a46f4d5af9456247e
Volkan24 <vkaral04@hotmail.com>
2021-09-24 08:26:57 +0000

    infoles slotbegin

---
+++ game.js
@@ -0,0 +1,35 @@
+class Game{
+    nummer;
+
+    constructor(_nummer){
+        this.nummer = _nummer;
+    }
+
+
+    setup(){
+
+  createCanvas(1280, 720);
+
+}
+
+draw(){
+    
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ slot.js
@@ -0,0 +1,23 @@
+class Slot extends Game{
+    x;
+    y;
+    widthX;
+    widthY;
+
+
+constructor(_x,_y,_widthX,_widthY,_nummer){
+    super(_nummer);
+    
+    this.x = _x;
+    this.y = _y;
+    this.widthX = _widthX;
+    this.widthY = _widthY;
+}
+
+
+
+
+
+
+
+}
